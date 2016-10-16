13edfd4c8d7ce1c1079ad8607eac5aecbf50aa81
owner90 <owner90@owner90-PC>
2016-10-16 14:23:39 +0300

    From 1 to N console

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+
+		int i = 0;
+		System.out.println("Input Number: ");
+		int n = scan.nextInt();
+		
+		while(i<n){
+			i++;
+			System.out.println(i);
+		}
+	}
+			}
+			
